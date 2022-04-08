795efdc97c7890cf00a091eda1364c8faf559683
Binal Chhodvadiya <binalchhodvadiya12@gmail.com>
2022-04-09 02:28:39 +0530

    done withthe design

--- script.js
+++ script.js
@@ -4,3 +4,20 @@ resume.addEventListener('click', () => {
 
     window.open('./resume.pdf', '_blank').focus()
 })
+
+var goto = (p) => {
+    window.location.href = "#" + p.textContent
+}
+
+
+// ===============================================
+
+const project = document.getElementById("Projects").children;
+
+for( let i=2, n=project.length; i<n; i++){
+    if(i%2 == 0){
+        project[i].children[1].style.marginLeft = "55px"
+    } else {
+        project[i].children[0].style.marginRight = "55px"
+    }
+}
